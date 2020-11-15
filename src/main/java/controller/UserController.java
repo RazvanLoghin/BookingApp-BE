94f5160f9c2e5e32b3b7d1dae13f9d1f35e27da1
Mihaela Bizgan <bizganmihaela@gmail.com>
2020-11-15 14:18:09 +0100

    Commit2

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,22 +2,23 @@ package controller;
 
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController extends BaseController {
 
     private static final String API_NAME = "user";
-    private static final String URI = UserController.API_V1 + "/" +
-            UserController.API_NAME;
 
-    @Autowired
-    public UserController() {
+    @GetMapping(UserController.API_NAME + "/{id}")
+    public ResponseEntity<String> getUser(@PathVariable long id) {
+        return ResponseEntity.ok("user1");
+    }
 
+    @GetMapping(UserController.API_NAME)
+    public ResponseEntity<String> getAllUsers() {
+        return ResponseEntity.ok("All users!");
     }
 
     @GetMapping("/hello")
@@ -26,9 +27,19 @@ public class UserController extends BaseController {
     }
 
     @PostMapping(UserController.API_NAME)
-    public ResponseEntity<String> createNewUser(@RequestBody User user) {
-        createNewUser(user);
+    public ResponseEntity<String> createNewUser() {
         return ResponseEntity.ok("New User created!");
+    }
+
+    @PutMapping(UserController.API_NAME + "/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable long id){
+        return new ResponseEntity("User " + id + " has been updated!", HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping(UserController.API_NAME + "/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable long id) {
+        return new ResponseEntity<>("User " + id + " has been deleted!", HttpStatus.NOT_FOUND);
+
     }
 }
 
