9a1481e9ff908c35e9ef1f7596ad6d51165176da
Razvan Loghin <razvanloghin@Razvans-MacBook-Air.local>
2020-11-15 10:16:57 +0200

    Initial commit

---
+++ src/main/java/com/booking/app/BookingAppBeApplication.java
@@ -0,0 +1,15 @@
+package com.booking.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAppBeApplication.class, args);
+
+		System.out.println("Starting the app...");
+	}
+
+}
