5a5849cb0702d014e03f1a3512b177db819b4976
Mihaela Bizgan <bizganmihaela@gmail.com>
2020-12-05 13:29:26 +0100

    final changes for Stefan

--- src/main/java/com/booking/app/BookingAppBeApplication.java
+++ src/main/java/com/booking/app/BookingAppBeApplication.java
@@ -2,16 +2,14 @@ package com.booking.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableJpaRepositories
 public class BookingAppBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingAppBeApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppBeApplication.class, args);
 
-		System.out.println("Starting the app...");
-	}
+        System.out.println("Starting the app...");
+    }
 
 }
